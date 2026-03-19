182de822c251a80a8f774e49f8e2d7ec4699881d
D0do777 <justeforbusiness1@gmail.com>
2026-03-19 19:50:23 +0100

    Update controls.js

--- js/controls.js
+++ js/controls.js
@@ -1,18 +1,13 @@
 const keys = {};
 
 document.addEventListener("keydown", e => {
+    keys[e.key.toLowerCase()] = true;
 
-keys[e.key.toLowerCase()] = true;
-
-if(e.code === "Space")
-{
-player.gravityDir *= -1;
-}
-
+    if (e.code === "Space") {
+        player.gravityDir *= -1;
+    }
 });
 
 document.addEventListener("keyup", e => {
-
-keys[e.key.toLowerCase()] = false;
-
+    keys[e.key.toLowerCase()] = false;
 });
