aa2b2e345e1ec1897c276197339071c4791e18b9
D0do777 <justeforbusiness1@gmail.com>
2026-03-19 19:51:07 +0100

    Update coin.js

--- js/coin.js
+++ js/coin.js
@@ -1,24 +1,13 @@
 const coins = [
-
-{x:500,y:420,width:16,height:16,collected:false},
-{x:750,y:420,width:16,height:16,collected:false}
-
+    { x: 500, y: 420, width: 16, height: 16, collected: false },
+    { x: 750, y: 420, width: 16, height: 16, collected: false }
 ];
 
-function updateCoins()
-{
-
-for(let c of coins)
-{
-
-if(!c.collected && collision(player,c))
-{
-
-c.collected = true;
-player.coins++;
-
-}
-
-}
-
+function updateCoins() {
+    for (let c of coins) {
+        if (!c.collected && collision(player, c)) {
+            c.collected = true;
+            player.coins++;
+        }
+    }
 }
