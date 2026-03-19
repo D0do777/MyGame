eec2aa8d165c718cfdddcda7bf304274bcee7489
D0do777 <justeforbusiness1@gmail.com>
2026-03-19 19:50:52 +0100

    Update enemy.js

--- js/enemy.js
+++ js/enemy.js
@@ -1,28 +1,16 @@
 const enemies = [
-
-{x:600,y:460,width:32,height:32,dir:1}
-
+    { x: 600, y: 460, width: 32, height: 32, dir: 1 }
 ];
 
-function updateEnemies()
-{
+function updateEnemies() {
+    for (let e of enemies) {
+        e.x += e.dir * 2;
 
-for(let e of enemies)
-{
+        if (e.x < 500 || e.x > 900) e.dir *= -1;
 
-e.x += e.dir * 2;
-
-if(e.x < 500 || e.x > 900)
-e.dir *= -1;
-
-if(collision(player,e))
-{
-
-alert("Game Over");
-location.reload();
-
-}
-
-}
-
+        if (collision(player, e)) {
+            alert("Game Over");
+            location.reload();
+        }
+    }
 }
