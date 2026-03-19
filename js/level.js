4ee40b3c4017a09a7d1f0229e998d9ac3f7064fe
D0do777 <justeforbusiness1@gmail.com>
2026-03-19 19:33:37 +0100

    Update level.js

--- js/level.js
+++ js/level.js
@@ -28,31 +28,23 @@ ctx.translate(-camera.x,-camera.y);
 
 for(let p of platforms)
 {
-
 ctx.fillStyle="gray";
 ctx.fillRect(p.x,p.y,p.width,p.height);
-
 }
 
 for(let c of coins)
 {
-
 if(!c.collected)
 {
-
 ctx.fillStyle="yellow";
 ctx.fillRect(c.x,c.y,c.width,c.height);
-
 }
-
 }
 
 for(let e of enemies)
 {
-
 ctx.fillStyle="red";
 ctx.fillRect(e.x,e.y,e.width,e.height);
-
 }
 
 ctx.fillStyle="cyan";
@@ -60,10 +52,8 @@ ctx.fillRect(player.x,player.y,player.width,player.height);
 
 for(let l of lasers)
 {
-
 ctx.fillStyle="lime";
-ctx.fillRect(l.x,l.y,8,3);
-
+ctx.fillRect(l.x,l.y,10,3);
 }
 
 ctx.restore();
