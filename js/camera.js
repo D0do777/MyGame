7d545239a4c7c3e1856fe2c3ec3151f460fe6446
D0do777 <justeforbusiness1@gmail.com>
2026-03-19 19:51:36 +0100

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -1,16 +1,12 @@
-const camera =
-{
-x:0,
-y:0
+const camera = {
+    x: 0,
+    y: 0
 };
 
-function updateCamera()
-{
+function updateCamera() {
+    camera.x = player.x - canvas.width / 2;
+    camera.y = player.y - canvas.height / 2;
 
-camera.x = player.x - canvas.width/2;
-camera.y = player.y - canvas.height/2;
-
-if(camera.x < 0) camera.x = 0;
-if(camera.y < 0) camera.y = 0;
-
+    if (camera.x < 0) camera.x = 0;
+    if (camera.y < 0) camera.y = 0;
 }
