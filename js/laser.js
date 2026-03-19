2a87f31b8ebe1fdccca6eb67f22846f2d82c425a
D0do777 <justeforbusiness1@gmail.com>
2026-03-19 19:51:24 +0100

    Update laser.js

--- js/laser.js
+++ js/laser.js
@@ -1,37 +1,21 @@
 const lasers = [];
 
 document.addEventListener("mousedown", e => {
-
-lasers.push({
-
-x:player.x + player.width/2,
-y:player.y + player.height/2,
-vx:10
-
+    lasers.push({
+        x: player.x + player.width / 2,
+        y: player.y + player.height / 2,
+        vx: 10
+    });
 });
 
-});
+function updateLasers() {
+    for (let l of lasers) {
+        l.x += l.vx;
 
-function updateLasers()
-{
-
-for(let l of lasers)
-{
-
-l.x += l.vx;
-
-for(let e of enemies)
-{
-
-if(collision(l,e))
-{
-
-enemies.splice(enemies.indexOf(e),1);
-
-}
-
-}
-
-}
-
+        for (let e of enemies) {
+            if (collision(l, e)) {
+                enemies.splice(enemies.indexOf(e), 1);
+            }
+        }
+    }
 }
